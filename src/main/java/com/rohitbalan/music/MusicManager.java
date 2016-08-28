98c6f8ac37a7747621ed842a69f220a6ff4c7d73
Rohit Balan <rohitbalan82@gmail.com>
2016-08-27 22:05:29 -0400

    Cleanup

--- src/main/java/com/rohitbalan/music/MusicManager.java
+++ src/main/java/com/rohitbalan/music/MusicManager.java
@@ -11,28 +11,19 @@ public class MusicManager
 {
     public static void main(String[] args)
     {
-        for(String arg: args) {
-        	try {
-				List<Track> tracks = new Parser(arg).execute();
-				
-				for(final Track track: tracks) {
-					/*new Thread(){
-						@Override
-						public void run() {
-							try {
-								Thread.sleep(10000);
-							} catch (InterruptedException e) {
-								e.printStackTrace();
-							}
-							new Downloader().download(track);
-						}
-						
-					}.start();*/
-					new Downloader().download(track);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-        }
+    	if(args!=null) {
+            for(String arg: args) {
+            	try {
+    				List<Track> tracks = new Parser(arg).execute();
+    				
+    				for(final Track track: tracks) {
+    					new Downloader().download(track);
+    				}
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+            }
+    	}
+		System.out.println("Exiting");
     }
 }
